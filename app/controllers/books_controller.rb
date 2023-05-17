b687a1c03f7e683cdcd41fdaa9f14279e4f3e131
tompi04 <126743702+tompi04@users.noreply.github.com>
2023-05-17 08:52:50 +0000

    [Update] controllerファイルの作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @book.user_id = current_user.id
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
