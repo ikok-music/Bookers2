da793e9bb08a35e74c9ee9b79f2d5a7434aca39a
tompi04 <126743702+tompi04@users.noreply.github.com>
2023-05-17 15:58:09 +0000

    [Update] userのshowページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books.all
+    @user_info = Book.new
+    @user_info.user_id = @user.id
   end
 
   def edit
