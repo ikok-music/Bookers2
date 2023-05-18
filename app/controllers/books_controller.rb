3aacb5ffe3e90c6cc7d2672b37128824caff2af7
tompi04 <126743702+tompi04@users.noreply.github.com>
2023-05-18 10:24:43 +0000

    [Update] bookのeditページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -22,6 +23,9 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
