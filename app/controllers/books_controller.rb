9c6b108cde019fefe8a2c26e9ed9ebd15bb2e154
tompi04 <126743702+tompi04@users.noreply.github.com>
2023-05-19 03:54:31 +0000

    [Update] 新規投稿が失敗したとき

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,11 @@
 class BooksController < ApplicationController
   def index
-    @user_info = Book.new
-    @user_info.user_id = current_user.id
+    @book = Book.new
+    @book.user_id = current_user.id
     @books = Book.all
   end
 
   def show
-    @user_info = Book.new
-    @user_info.user_id = current_user.id
     @book = Book.find(params[:id])
   end
 
@@ -18,9 +16,15 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user_info = Book.new
+      @user_info.user_id = current_user.id
+      @books = Book.all
+      render :index
+    end
   end
 
   def update
@@ -41,5 +45,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   def index
-    @user_info = Book.new
-    @user_info.user_id = current_user.id
+    @book = Book.new
+    @book.user_id = current_user.id
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books.all
-    @user_info = Book.new
-    @user_info.user_id = @user.id
+    @book = Book.new
+    @book.user_id = @user.id
   end
 
   def edit
